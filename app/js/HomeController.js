e6571459b846c99610b8b3f98def4c57c1056aa2
unknown <czhang@czhang-pc01.rock-ny.rockportllc.com>
2015-10-04 14:59:22 -0400

    more clean up

--- app/js/HomeController.js
+++ app/js/HomeController.js
@@ -1,5 +1,13 @@
 angular.module('app').controller('HomeController', function ($state) {
     var ctrl = this;
 
+
+    ctrl.istFeed = [];
+
+    for(var i=0; i<50; i++){
+    	var x = i%7 + 1;
+    	ctrl.istFeed.push('img/style'+x+'.jpg');
+    }
+
     //console.log('HomeController');
 });
