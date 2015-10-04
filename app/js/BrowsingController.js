43e7a21ff702f2e0265f2886dac1800eee19fa0b
unknown <czhang@czhang-pc01.rock-ny.rockportllc.com>
2015-10-03 20:17:57 -0400

    stylist page

--- app/js/BrowsingController.js
+++ app/js/BrowsingController.js
@@ -5,7 +5,7 @@ angular.module('app').controller('BrowsingController', function ($state) {
 
     var tiers = ['1', '2', '3'];
     var events = ['Wedding', 'Interview'];
-    //var ratings = ['5 star', '4 star', '5 star', '5 star']
+    var ratings = [5,4,3,2,1];
 
     ctrl.filterOptions = [
     	{name: 'tier', category: 'Tier', options: tiers},
@@ -14,15 +14,26 @@ angular.module('app').controller('BrowsingController', function ($state) {
     	{name: 'event', category: 'Event', options: events}
     ];
 
+    ctrl.sortings = [{disply: 'tier', sort: 'tier'}, {disply:'rating', sort:'-rating'}];
+
 
     ctrl.stylists = [];
-    for(var i=0;i<10;i++){
+    for(var i=0;i<12;i++){
     	var tier = Math.floor(Math.random() * tiers.length);
     	var event = Math.floor(Math.random() * events.length);
+    	var rating = Math.floor(Math.random() * ratings.length);
 
     	var img = Math.floor(Math.random() * 4) + 1;
+    	var icon = Math.floor(Math.random() * 4) + 1;
 
-    	ctrl.stylists.push({img:'img/product'+img+'.jpg', tier: tiers[tier], event: events[event]});
+    	ctrl.stylists.push({
+    		img:'img/product'+img+'.jpg', 
+    		icon:'img/product'+icon+'.jpg', 
+    		tier: tiers[tier], 
+    		event: events[event],
+    		rating: ratings[rating],
+    		name: 'stylist name'
+    	});
     }
 
 
